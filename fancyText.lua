78ca4547bfc40dff75da09e50c22512feed82453
Milo:3 Silli Denote <71358745+MiloX3-Silli-Denote@users.noreply.github.com>
2025-09-10 14:28:54 -0400

    Update fancyText.lua

--- fancyText.lua
+++ fancyText.lua
@@ -2,8 +2,9 @@ local FancyText = {};
 FancyText.__index = FancyText;
 
 FancyText.font = PIXEL_FONT_128;--love.graphics.newFont("SpaceMono.ttf", 128);
-FancyText.fontHeight = FancyText.font:getHeight("|");
-FancyText.spaceWidth = FancyText.font:getWidth(" ");
+FancyText.fontHeight = FancyText.font:getHeight();
+
+FancyText.defaultFont = PIXEL_FONT_128;
 
 FancyText.DEFAULT_COLOURS = {
     ["red"] = {1,0,0};
@@ -27,7 +28,7 @@ FancyText.DEFAULT_COLOURS = {
     ["off"] = {1,1,1,0};
 };
 
-function FancyText.new(text, x, y, width, textHeight, alignment, dataReference)
+function FancyText.new(text, x, y, width, textHeight, alignment, font, dataReference)
     local instance = setmetatable({}, FancyText);
 
     instance.x = x;
@@ -43,9 +44,21 @@ function FancyText.new(text, x, y, width, textHeight, alignment, dataReference)
 
     instance.pointer = dataReference or {};
 
+    instance.font = font or FancyText.defaultFont;
+    instance.fontHeight = font and font:getHeight() or FancyText.fontHeight;
+
+    instance.highlighterEdge = 5;
+
+    instance.height = 0; -- needs to be calculated
+
     instance:alignText();
 
     return instance;
+end
+
+-- changes the radius of the highlighter corners
+function FancyText:setHighLighterEdge(newEdge)
+    self.highlighterEdge = newEdge;
 end
 
 function FancyText:setPosition(x, y)
@@ -107,17 +120,22 @@ function FancyText:alignText()
         end
     );
 
+    local lineHeight = 0;
     local lineWidth = 0;
     local scale = self.textHeight / self.fontHeight;
 
     self.lines = {}; -- empty the table
+    self.height = 0; -- reset height to be calculated again
 
     local curLine = "";
     local curAddLine = {};
 
-    while string.len(realText) > 0 do
-        local tillNextSpace, rest = string.match(realText, "^([^ \n]*[ \n])(.*)$");
+    local curFont = self.font; -- what the current font is while printing
 
+    while string.len(realText) > 0 do
+        local tillNextSpace, rest = string.match(realText, "^([^%s\n]*[%s\n])(.*)$"); -- get the text up to the next space and split it there
+
+        -- if there is not any spaces left in the text then just split it at the end of the text
         if not tillNextSpace then
             tillNextSpace = realText;
             realText = "";
@@ -127,67 +145,111 @@ function FancyText:alignText()
 
         local tempAddLine = {};
 
-        while string.find(tillNextSpace, specialCharacter, 1, true) do
-            local ind = string.find(tillNextSpace, specialCharacter, 1, true);
-            tillNextSpace = string.sub(tillNextSpace, 0, ind - 1) .. string.sub(tillNextSpace, ind + 1,-1);
+        local widthOfStr = 0; -- start remembering the width of this line
 
+        local parsedText = ""; -- if we change the font then we need to store the previously calculated text
+
+        -- while there are graphical changes in the string up till the next space
+        while string.find(tillNextSpace, specialCharacter, 1, true) do -- final arg is true 2 prevent 'specialCharacter' from accidentally performing a regex
             local modif = table.remove(graphicalChanges, 1);
+
+            local ind = string.find(tillNextSpace, specialCharacter, 1, true); -- get the amount of characters b4 the graphical change
+
+            -- if the graphical change we just found is a font change then we need to alter the calculation for the width of the line
+            if string.find(modif, "^font=.*$") then
+                local previousFontText = string.sub(tillNextSpace, 0, ind - 1);
+
+                widthOfStr = widthOfStr + curFont:getWidth(previousFontText) * scale; -- add the width of the previous text to the width of the current line
+
+                curFont = self.pointer[(string.match(modif, "^font=(.*)$"))];
+
+                if not curFont then
+                    if (string.match(modif, "^font=(.*)$")) == "default" then
+                        curFont = self.font;
+                    end
+                end
+
+                assert(curFont and curFont:type() == "Font", "tried to set font to a non font object");
+
+                parsedText = parsedText .. previousFontText;
+                tillNextSpace = string.sub(tillNextSpace, ind + 1,-1); -- remove the graphical change from the text
+            else -- if the graphical change is not a font change then dont alter anything
+                tillNextSpace = string.sub(tillNextSpace, 0, ind - 1) .. string.sub(tillNextSpace, ind + 1,-1); -- remove the graphical change from the text
+            end
 
             table.insert(tempAddLine, {charStart = ind, modificationName = string.match(modif, "^[^=]*"), setTo = string.match(modif, "[^=]*$")});
         end
 
-        local widthOfStr = self.font:getWidth(string.match(tillNextSpace, "^[^ \n]*")) * scale; -- remove space for measurement
+        widthOfStr = widthOfStr + curFont:getWidth(string.match(tillNextSpace, "^[^%s\n]*")) * scale; -- remove space for measurement
 
+        tillNextSpace = parsedText .. tillNextSpace; -- add previously parsed text after we calculated the width of the non-parsed text
+
+        -- check if this string can be added to the current line, or if it will overflow the provided width
         if lineWidth + widthOfStr > self.width then
-            curAddLine.text = (string.match(curLine, "^(.*)[ \n]$")) or curLine;
+            -- add the current line to the list of lines
+            curAddLine.text = (string.match(curLine, "^(.*)[%s\n]$")) or curLine;
+            curAddLine.width = lineWidth;
+            curAddLine.height = lineHeight;
             table.insert(self.lines, curAddLine);
             curAddLine = {};
 
-            for i, v in ipairs(tempAddLine) do
+            -- add this string to the next line
+            for _, v in ipairs(tempAddLine) do
                 table.insert(curAddLine, v);
             end
 
             curLine = tillNextSpace;
-            lineWidth = widthOfStr + self.spaceWidth * scale;
+            lineWidth = widthOfStr + curFont:getWidth(string.sub(tillNextSpace, -1,-1)) * scale;
+            lineHeight = curFont:getHeight() * scale;
         else
-            for i, v in ipairs(tempAddLine) do
+            for _, v in ipairs(tempAddLine) do
                 v.charStart = v.charStart + string.len(curLine);
                 table.insert(curAddLine, v);
             end
 
             curLine = curLine .. tillNextSpace;
-            lineWidth = lineWidth + widthOfStr + self.spaceWidth * scale;
+            lineWidth = lineWidth + widthOfStr + curFont:getWidth(string.sub(tillNextSpace, -1,-1)) * scale;
+            lineHeight = math.max(lineHeight, curFont:getHeight() * scale);
         end
 
         -- check if line ended with a line break
         if string.find(tillNextSpace, "\n") then
-            curAddLine.text = (string.match(curLine, "^(.*)[ \n]$")) or curLine;
+            curAddLine.text = (string.match(curLine, "^(.*)[%s\n]$")) or curLine;
+            curAddLine.width = lineWidth - curFont:getWidth(string.sub(tillNextSpace, -1,-1)) * scale;
+            curAddLine.height = lineHeight;
             table.insert(self.lines, curAddLine);
             curAddLine = {};
 
             curLine = "";
             lineWidth = 0;
+            lineHeight = 0;
         end
     end
 
     curAddLine.text = (string.match(curLine, "^(.*)[ \n]$")) or curLine;
+    curAddLine.width = lineWidth;
+    curAddLine.height = lineHeight;
     table.insert(self.lines, curAddLine);
 end
 
 function FancyText:getHeight()
-    return #self.lines * self.textHeight;
+    local height = 0;
+
+    for _, v in ipairs(self.lines) do
+        height = height + v.height;
+    end
+
+    return height;
 end
 
 function FancyText:getWidth()
-    local scale = self.textHeight / self.fontHeight;
-
-    local longest = self.font:getWidth(self.lines[1].text);
+    local longest = self.lines[1].width;
 
     for i = 2, #self.lines do
-        longest = math.max(self.font:getWidth(self.lines[i].text), longest);
+        longest = math.max(self.lines[i].width, longest);
     end
 
-    return longest * scale;
+    return longest;
 end
 
 function FancyText:draw()
@@ -200,44 +262,56 @@ function FancyText:draw()
     love.graphics.translate(self.x, self.y);
     love.graphics.setFont(self.font);
 
+    local curFont = self.font; -- what the current font is while printing
+    local y = 0;
+
     -- has some bad coding practices but is very hard to fix without creating unecessary public functions
     -- or extremely inefficient local functions that are created and destroyed every draw call
-    for i, v in ipairs(self.lines) do
-        local lineWidth = self.font:getWidth(v.text) * scale;
-
+    for _, v in ipairs(self.lines) do
         local x;
 
         if self.alignment == "left" then
             x = 0;
         elseif self.alignment == "middle" or self.alignment == "center" then
-            x = (self.width - lineWidth) / 2;
+            x = (self.width - v.width) / 2;
         elseif self.alignment == "right" then
-            x = self.width - lineWidth;
+            x = self.width - v.width;
         else
             x = 0;
         end
 
+        -- if there are any graphical changes inside of this line
         if #v > 0 then
             local alreadyDrawn = 1;
 
-            for j, w in ipairs(v) do
+            for _, w in ipairs(v) do
                 local toDraw = string.sub(v.text, alreadyDrawn, w.charStart - 1);
                 alreadyDrawn = w.charStart;
 
                 if highlight[4] ~= 0 then -- if alpha of the colour is not 0 then draw the highlight
                     love.graphics.setColor(highlight);
-                    love.graphics.rectangle("fill", x - 2, (i - 1) * self.fontHeight * scale, self.font:getWidth(toDraw) * scale + 4, self.fontHeight * scale, 3,3);
+                    love.graphics.rectangle("fill", x - 2, y + v.height - curFont:getHeight() * scale, curFont:getWidth(toDraw) * scale + 4, curFont:getHeight() * scale, self.highlighterEdge);
                 end
 
                 love.graphics.setColor(colour);
-                love.graphics.print(toDraw, x, (i - 1) * self.fontHeight * scale, 0, scale,scale);
+                love.graphics.print(toDraw, x, y + v.height - curFont:getHeight() * scale, 0, scale,scale);
 
-                x = x + self.font:getWidth(toDraw) * scale;
+                x = x + curFont:getWidth(toDraw) * scale;
 
                 if w.modificationName == "colour" or w.modificationName == "color" then
                     colour = self.DEFAULT_COLOURS[w.setTo];
                 elseif w.modificationName == "highlight" then
                     highlight = self.DEFAULT_COLOURS[w.setTo];
+                elseif w.modificationName == "font" then
+                    curFont = self.pointer[w.setTo];
+
+                    if not curFont then
+                        if w.setTo == "default" then
+                            curFont = self.font;
+                        end
+                    end
+
+                    love.graphics.setFont(curFont);
                 end
             end
 
@@ -245,20 +319,22 @@ function FancyText:draw()
 
             if highlight[4] ~= 0 then -- if alpha of the colour is not 0 then draw the highlight
                 love.graphics.setColor(highlight);
-                love.graphics.rectangle("fill", x - 2, (i - 1) * self.fontHeight * scale, self.font:getWidth(toDraw) * scale + 4, self.fontHeight * scale, 3,3);
+                love.graphics.rectangle("fill", x - 2, y + v.height - curFont:getHeight() * scale, curFont:getWidth(toDraw) * scale + 4, curFont:getHeight() * scale, self.highlighterEdge);
             end
 
             love.graphics.setColor(colour);
-            love.graphics.print(toDraw, x, (i - 1) * self.fontHeight * scale, 0, scale,scale);
-        else
+            love.graphics.print(toDraw, x, y + v.height - curFont:getHeight() * scale, 0, scale,scale);
+        else -- if there are ZERO graphical changes in this line
             if highlight[4] ~= 0 then -- if alpha of the colour is not 0 then draw the highlight
                 love.graphics.setColor(highlight);
-                love.graphics.rectangle("fill", x - 2, (i - 1) * self.fontHeight * scale + 4, lineWidth, self.fontHeight * scale, 3,3);
+                love.graphics.rectangle("fill", x - 2, y, v.width + 4, v.height, self.highlighterEdge);
             end
 
             love.graphics.setColor(colour);
-            love.graphics.print(v.text, x, (i - 1) * self.fontHeight * scale, 0, scale,scale);
+            love.graphics.print(v.text, x, y, 0, scale,scale);
         end
+
+        y = y + v.height; -- move the next line downwards by the height of this line
     end
 
     love.graphics.pop();
